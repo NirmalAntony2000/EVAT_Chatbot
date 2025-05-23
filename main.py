d12634c330331f9c80a01de6ec45f74773ce384e
NirmalAntony2000 <167950410+NirmalAntony2000@users.noreply.github.com>
2025-05-23 10:42:20 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or restrict to your frontend domain
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
